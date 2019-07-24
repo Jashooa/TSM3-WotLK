525dbcd884a5bd11786a664917916a8cc38b9cc3
Joshua Stachyshyn <Jashooa@gmail.com>
2019-07-24 19:27:49 +1200

    fix missing class colours

--- TradeSkillMaster/TradeSkillMaster.lua
+++ TradeSkillMaster/TradeSkillMaster.lua
@@ -615,7 +615,20 @@ function TSM:LoadTooltip(itemString, quantity, moneyCoins, lines)
 				tinsert(lines, {left="  "..L["Custom Price Source"].." '"..name.."':", right=TSMAPI:MoneyToString(price*quantity, "|cffffffff", "OPT_PAD", moneyCoins and "OPT_ICON" or nil)})
 			end
 		end
-	end
+    end
+
+    RAID_CLASS_COLORS = {
+        ["HUNTER"] = { r = 0.67, g = 0.83, b = 0.45, colorStr = "ffabd473" },
+        ["WARLOCK"] = { r = 0.53, g = 0.53, b = 0.93, colorStr = "ff8788ee" },
+        ["PRIEST"] = { r = 1.0, g = 1.0, b = 1.0, colorStr = "ffffffff" },
+        ["PALADIN"] = { r = 0.96, g = 0.55, b = 0.73, colorStr = "fff58cba" },
+        ["MAGE"] = { r = 0.25, g = 0.78, b = 0.92, colorStr = "ff3fc7eb" },
+        ["ROGUE"] = { r = 1.0, g = 0.96, b = 0.41, colorStr = "fffff569" },
+        ["DRUID"] = { r = 1.0, g = 0.49, b = 0.04, colorStr = "ffff7d0a" },
+        ["SHAMAN"] = { r = 0.0, g = 0.44, b = 0.87, colorStr = "ff0070de" },
+        ["WARRIOR"] = { r = 0.78, g = 0.61, b = 0.43, colorStr = "ffc79c6e" },
+        ["DEATHKNIGHT"] = { r = 0.77, g = 0.12 , b = 0.23, colorStr = "ffc41f3b" },
+    };
 
 	-- add inventory information
 	if TSM.db.profile.inventoryTooltipFormat == "full" then

85c2bab1921b22ea42c92826dd7df8eef17757f4
Joshua Stachyshyn <Jashooa@gmail.com>
2019-07-25 00:10:02 +1200

    fix auction posting

--- TradeSkillMaster_Shopping/modules/AuctionTab.lua
+++ TradeSkillMaster_Shopping/modules/AuctionTab.lua
@@ -426,9 +426,8 @@ function private.PostAuctionsThread(self, auctionInfo)
 	local postDuration = postFrame.durationDropdown:GetValue() or 2
 
 	auctionBuyout = floor(auctionBuyout / auctionRecord.stackSize) * min(auctionRecord.stackSize, numInBags)
-	local postInfo = { buyout = auctionBuyout, stackSize = min(auctionRecord.stackSize, numInBags), numInBags = numInBags, numStacks = 1, duration = postDuration, updateBuyout = true, isDonePosting = nil, itemLink = TSMAPI.Item:GetLink(inventoryItemString), rawItemLink = inventoryRawItemLink }
+	local postInfo = { buyout = auctionBuyout, stackSize = min(auctionRecord.stackSize, numInBags), numInBags = numInBags, numStacks = 1, duration = postDuration, updateBuyout = true, itemLink = TSMAPI.Item:GetLink(inventoryItemString), rawItemLink = inventoryRawItemLink }
 	private.frame.UpdateConfirmation("post", auctionRecord, postInfo)
-	self:RegisterEvent("BAG_UPDATE", function() self:SendMsgToSelf("BAG_UPDATE") end)
 	self:RegisterEvent("CHAT_MSG_SYSTEM", function(_, msg) if postInfo.numStacks == 1 and msg == ERR_AUCTION_STARTED then self:SendMsgToSelf("AUCTION_POSTED", 1) end end)
 	local lastMultipostQuantity = 0
 	self:RegisterEvent("AUCTION_MULTISELL_UPDATE", function(_, arg1, arg2)
@@ -479,12 +478,6 @@ function private.PostAuctionsThread(self, auctionInfo)
 			else
 				postFrame.postBtn:Disable()
 			end
-		elseif event == "BAG_UPDATE" then
-			-- update the number in the player's bags
-			postInfo.numInBags = TSM.AuctionTabUtil:GetNumInBags(auctionRecord.itemString)
-			if postInfo.numInBags == 0 or postInfo.isDonePosting then
-				break -- done posting
-			end
 		elseif event == "INTERRUPTED" then
 			-- posting was stopped
 			break
@@ -509,7 +502,7 @@ function private.PostAuctionsThread(self, auctionInfo)
 				private.frame.content.result.rt:RemoveSelectedRecord(1)
 			end
 			private.frame.content.result.rt:InsertAuctionRecord(numStacksPosted, record)
-			postInfo.isDonePosting = true
+            break
 		else
 			error("Unexpected message: " .. tostring(event))
 		end

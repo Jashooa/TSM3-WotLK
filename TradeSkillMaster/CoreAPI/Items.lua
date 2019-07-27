1400c0b2837238432804c3dc38b214064222d7d8
Joshua Stachyshyn <Jashooa@gmail.com>
2019-07-27 20:22:29 +1200

    fix missing soulboundCache

--- TradeSkillMaster/CoreAPI/Items.lua
+++ TradeSkillMaster/CoreAPI/Items.lua
@@ -11,7 +11,7 @@
 local TSM = select(2, ...)
 local Items = TSM:NewModule("Items", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("TradeSkillMaster") -- loads the localization table
-local private = {itemInfo={}, scanTooltip=nil, pendingItems={}}
+local private = {itemInfo={}, scanTooltip=nil, pendingItems={}, soulboundCache={}}
 local STATIC_DATA = {classLookup={}, classIdLookup={}, inventorySlotIdLookup={}}
 
 for classId, class in pairs({GetAuctionItemClasses()}) do
